caf33f40f0e61327265c84be19a19720e8b65934
shrawanijogi6-alt <shrawanijogi6@gmail.com>
2025-10-31 00:40:36 +0530

    first commit

---
+++ a11.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Determine the maximum using a simple if statement
+if num1 > num2:
+print(f"The maximum number is: {num1}")
+if num2 > num1:
+print(f"The maximum number is: {num2}")
