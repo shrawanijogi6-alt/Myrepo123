c0332d3d84e903e858abcd3b2f5b0464325698f9
shrawanijogi6-alt <shrawanijogi6@gmail.com>
2025-11-01 00:47:51 +0530

    first commit

---
+++ a13.py
@@ -0,0 +1,7 @@
+n = int(input('Enter a number : '))
+
+i - 1 
+while i <=10;
+    K = n * 1
+    print(f'{n} x {i} = {K}')
+    i+=1
---
+++ a15.py
@@ -0,0 +1,8 @@
+colors = ['green','red','yellow','pink','blue']
+data = (23,56,89,31,57)
+
+for c in colors:
+    print(c)
+for d in data:
+    print(d)
+    
